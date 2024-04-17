158b476942d6d4c45d05a08384b7ea06cf00f61c
unknown <porgsgomeep@gmail.com>
2024-04-17 16:03:18 -0700

    New animation background approach

--- components/pagetop.js
+++ components/pagetop.js
@@ -1,18 +1,17 @@
 import styles from "/styles/pagetop.module.css";
 import { useEffect } from "react";
-import { testanim } from "/Utils/anims.js";
+import { stripeScroll } from "/Utils/anims.js";
+import { textScroll } from "/Utils/anims.js";
+import { svg } from "/components/atsymbol.js";
 
 export default function Pagetop() {
   useEffect (() => {
-    testanim();
+    stripeScroll();
   }, [])
   return (
     <section className={styles.section}>
-        <h1 className={`${styles.boxtext} ${styles.h1} ${styles.bebasneue}`} id={"striped"}>Innovation</h1>
-      <div>
-        <h1 className={`${styles.h1} ${styles.bebasneue}`}>Innovation</h1>
-      </div>
-      <h2 className={styles.h2}>KLS Pitch Day</h2>
+      <h1 className={`${styles.h1} ${styles.bebasneue}`} id={"striped"}>Innovate</h1>
+      <h2 className={`${styles.h2} ${styles.prataregular}`}>KLS Pitch Day</h2>
     </section>
   );
 }
