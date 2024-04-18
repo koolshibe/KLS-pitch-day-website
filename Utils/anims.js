948cf25c719712b1f5b30c83d960e6b38a894c60
unknown <porgsgomeep@gmail.com>
2024-04-17 17:00:32 -0700

    ANIMATION WORKSS!!!!

--- Utils/anims.js
+++ Utils/anims.js
@@ -1,6 +1,21 @@
-import gsap from "gsap";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 export const stripeScroll = () => {
   const tl = new gsap.timeline({ repeat:-1 })
-  .to("#striped", { backgroundPosition:"0 28px", duration:1, ease:"none" });
+  .to("#striped", { backgroundPositionY:"28px", duration:10, ease:"none" });
+}
+
+export const mouseScroll = () => {
+  const tl2 = new gsap.timeline({ scrollTrigger: {
+    trigger:"#striped",
+    pin:true,
+    start: "top top",
+    end:"+=1000vw",
+    markers: true,
+    scrub: 1
+  }});
+  tl2.from("#striped", {backgroundSize:"100%", duration:1});
+  tl2.to("#striped", {backgroundSize:"0%", duration:1}).addLabel("end");
 }
--- components/pagetop.js
+++ components/pagetop.js
@@ -1,12 +1,13 @@
 import styles from "/styles/pagetop.module.css";
 import { useEffect } from "react";
 import { stripeScroll } from "/Utils/anims.js";
-import { textScroll } from "/Utils/anims.js";
+import { mouseScroll } from "/Utils/anims.js";
 import { svg } from "/components/atsymbol.js";
 
 export default function Pagetop() {
   useEffect (() => {
     stripeScroll();
+    mouseScroll();
   }, [])
   return (
     <section className={styles.section}>
